05ecd4bbd23abf65515da6bcccaf9170bfbf516a
André Luís <iftarodrigues@gmail.com>
2019-04-23 22:11:24 -0300

    primeira versao

---
+++ index.js
@@ -0,0 +1,17 @@
+const express= require("express")();
+const http = require("http").Server(express);
+const serverSocket = require("socket.io")(http);
+
+const porta= 8000;
+http.listen(porta, ()=>console.log("Servidor Iniciado em http://localhost:"+ porta));
+
+express.get("/", (req,res) => res.sendFile(__dirname+"/index.html"));
+
+serverSocket.on("connection", socket => {
+        console.log("Cliente "+socket.id+" conectado");
+        socket.on("chat msg", msg => {
+            console.log("Msg recebida do cliente: "+msg);
+            serverSocket.emit("chat msg", msg);
+        });
+    }
+);
